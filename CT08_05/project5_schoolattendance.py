ac1693a31998a028d464fa6fa416add9a122423a
Replit-is-bad <ylchia_hill@hotmail.com>
2026-02-13 11:00:37 +0000

    Co-authored-by: trainer1-computhink <trainer1-computhink@users.noreply.github.com>

--- CT08_05/project5_schoolattendance.py
+++ CT08_05/project5_schoolattendance.py
@@ -1,7 +1,3 @@
-def persentage_attendance(total_days, attended_days):
-    if total_days == 0:
-        return 0
-    return (attended_days / total_days) * 100
 
 def take_attendance(attendance_dictionary):
     for name,attendance_list in attendance_dictionary.items():
@@ -17,12 +13,24 @@ def take_attendance(attendance_dictionary):
                 print('invalid')
     return attendance
 
-def cal_attendance_persent(attendance_dictonary, student_name):
+def cal_attendance_percent(attendance_dictionary, student_name):
+    days_attended = 0
     if student_name in attendance_dictionary:
-        attendance_dictionary[student_name]
+        for attendance in attendance_dictionary[student_name]:
+            if attendance == True:
+                days_attended += 1 
+        return days_attended / len(attendance_dictionary[student_name]) * 100
     else:
         print(f'{student_name} not in class')
 
+def notify_low_attendance(attendance_dictionary, threshold):
+    low_attendance_student = []
+    for attendance in attendance_dictionary:
+        percent = cal_attendance_percent(attendance_dictionary, attendance)
+        if percent <= threshold:
+            print(f'WARNING {attendance} IS {percent}')
+            low_attendance_student.append(attendance)
+    return low_attendance_student
 
 attendance_dictionary = {
     "Matthew" : [],
@@ -41,6 +49,23 @@ attendance_dictionary = {
     "Isabelle" : []
 }
 
-print(attendance_dictionary)
-attendance_dictionary = take_attendance(attendance_dictionary)
-print(attendance_dictionary)
+while True:
+    print('SCHOOL ATTENDANCE SYSTEM')
+    print('1. Take Attendance')
+    print('2. CAlculate Attendance percent')
+    print('3. Notify Low attendance')
+    print('4. Exit program')
+    user_input = input('Select: ')
+    if user_input == "1":
+        take_attendance(attendance_dictionary)
+    elif user_input == "2":
+        student = input('Which student? ')
+        cal_attendance_percent(attendance_dictionary,student)
+    elif user_input == "3":
+        threshold1 = float(input('What is the threshold for low attendance? eg 10 :'))
+        notify_low_attendance(attendance_dictionary,threshold1)
+    elif user_input == "4":
+        print('im playing bloxed io')
+        break
+    else:
+        print("❌ Invalid choice.")
