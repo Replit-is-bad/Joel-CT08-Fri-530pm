a869e6962a015f77876af0ca02cb6dae2ca0b077
Replit-is-bad <ylchia_hill@hotmail.com>
2026-05-15 12:15:28 +0000

    home 1

--- CT08_14/project14_carshowroom.py
+++ CT08_14/project14_carshowroom.py
@@ -11,20 +11,29 @@ class Fighter :
         self.is_alive = True
 
     def attack(self, target):
+        # Random Damage Dealt
         damage = self.strength + random.randint(-self.strength,self.strength)
+        # Armour Defence
         damage_dealt = damage - target.armour
+        # Check if the damage is 0 or less than 0
         if damage_dealt <= 0 :
+            # Damage dealt to 0 so it will not heal enemy
+            damage_dealt == 0
+            print(f"{self.name} missed his attack!")
+        else:
+            # Critical Hit Chance
             critical = random.randint(1 , 100)
+            # 20% hit chance
             if critical > 80 :
-                damage_dealt * 1.5
-                print(f"{self.name} did a critical hit on {target.name}!")
+                # Critical hit x 2 damage
+                damage_dealt = damage_dealt * 2
+                if damage_dealt <= self.strength:
+                    damage_dealt * 1.7
+                print(f"{self.name} did a critical hit on {target.name} for {damage_dealt}!")
             else:
-                if damage_dealt >= 0:
-                    target.health -= damage_dealt
-                    print(f"{self.name} attacked {target.name} for {damage_dealt}")
-                else:
-                    damage_dealt == 0
-                    print(f"{self.name} missed his attack!")
+                # Normal attack
+                target.health -= damage_dealt
+                print(f"{self.name} attacked {target.name} for {damage_dealt}")
 
         print(f"{target.name} is now at {target.health}HP")
 
