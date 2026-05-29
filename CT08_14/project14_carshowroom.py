b597109a629276de5a328d43000f89824d7bd4c5
Replit-is-bad <ylchia_hill@hotmail.com>
2026-05-29 10:55:34 +0000

    last lession

--- CT08_14/project14_carshowroom.py
+++ CT08_14/project14_carshowroom.py
@@ -1,5 +1,7 @@
 import random
+import time
 class Fighter :
+
     def __init__(self, name, health, strength, armour, skill,mana):
         self.name = name
         self.health = health
@@ -8,12 +10,15 @@ class Fighter :
         self.skill = skill
         self.mana = mana
 
+        self.max_health = health
+        self.max_mana = mana
+        self.active_minion = None
+
         self.is_alive = True
 
     def attack(self, target):
         # Random Damage Dealt
-        damage = self.strength + random.randint(-self.strength,self.strength)
-        # Armour Defence
+        damage = random.randint(-5,self.strength)
         damage_dealt = damage - target.armour
         # Check if the damage is 0 or less than 0
         if damage_dealt <= 0 :
@@ -28,16 +33,105 @@ class Fighter :
                 # Critical hit x 2 damage
                 damage_dealt = damage_dealt * 2
                 if damage_dealt <= self.strength:
-                    damage_dealt * 1.7
+                    damage_dealt * 2
+                target.health -= damage_dealt
                 print(f"{self.name} did a critical hit on {target.name} for {damage_dealt}!")
             else:
                 # Normal attack
                 target.health -= damage_dealt
                 print(f"{self.name} attacked {target.name} for {damage_dealt}")
+            if self.active_minion:
+                self.active_minion.attack(target)
+        if target.health <= 0:
+            time.sleep(1)
+            print(f"{target.name} was brutally destroyed by {self.name}")
+        else:
+            time.sleep(1)
+            print(f"{target.name} is now at {target.health}HP")
 
-        print(f"{target.name} is now at {target.health}HP")
+    def use_skill(self, target):
+        if self.skill == "Sword Dance" and self.mana >= 5 :
+            self.mana -= 5
+            self.strength *= 2
+            time.sleep(1)
+            print(f"{self.name} used Sword Dance to increase attck by 100%" )
+        elif self.skill == "Summon Skeleton" and self.mana >= 65:
+            self.mana -= 65
+            self.active_minion = Minion("Skeleton", 5, 5)
+            time.sleep(1)
+            print(f"{self.name} used Summon Skeleton Minion to spawn one skeleton")
+        else:
+            time.sleep(1)
+            print("Not enough mana")
 
+    def rest(self):
+        if self.health >= self.max_health:
+            pass
+        else:
+            self.health += self.max_health * 0.2
+        self.mana += self.mana * 0.2
+        print(f"{self.name} rested")
+        print(f"health and mana restored")
+
+    def npc_turn(self, target):
+        npc_choices = ["attack", "skill", "rest"]
+        choice = random.choice(npc_choices)
+
+        if self.health < 25 and self.health > 0:
+            Bad_dude.rest()
+        else:
+            if choice == "attack":
+                Bad_dude.attack(My_dude)
+            elif choice == "skill":
+                if self.mana < 65:
+                    Bad_dude.attack(My_dude)
+
+
+class Minion:
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+
+        self.is_alive = True
+        self.active_minion = None
+    
+    def attack(self, target):
+        # Random Damage Dealt
+        damage = random.randint(0,self.strength)
+        damage_dealt = damage - target.armour
+        # Check if the damage is 0 or less than 0
+                # Normal attack
+        target.health -= damage_dealt
+        print(f"{self.name} attacked {target.name} for {damage_dealt}")
+
+#MAIN CODE
 My_dude = Fighter("Rusty", 70, 25, 10, "Sword Dance", 10)
-Bad_dude = Fighter("Mr.Necromancer", 125, 5, 5, "Skeleton Army", 200)
+Bad_dude = Fighter("Mr.Necromancer", 125, 5, 5, "Summon Skeleton", 200)
 
-My_dude.attack(Bad_dude)
+print(f"You have entered {Bad_dude.name}'s domain...")
+time.sleep(2)
+print("Prepare for battle!")
+time.sleep(1)
+round = 0
+while My_dude.health > 0 and Bad_dude.health > 0: 
+    round += 1
+
+    round_message = f"Round {round}"
+    print(round_message.center(20, "-"))
+    time.sleep(1)
+    print("1. attack")
+    print("2. Skill")
+    print("3. Rest")
+    player_input = input("ENTER YOUR CHOICE: ")
+
+    if player_input == "1":
+        My_dude.attack(Bad_dude)
+        Bad_dude.npc_turn(My_dude)
+    elif player_input == "2":
+        My_dude.use_skill(Bad_dude)
+        Bad_dude.npc_turn(My_dude)
+    elif player_input == "3":
+        My_dude.rest
+        Bad_dude.npc_turn(My_dude)
+    
--- CT08_15/project15_tamagotchi.py
+++ CT08_15/project15_tamagotchi.py
@@ -1,10 +1,107 @@
-class Fighter :
-    def __init__(self, name, health, strength, armour, skill,mana):
-        self.name = name
-        self.health = health
-        self.strength = strength
-        self.armour = armour
-        self.skill = skill
-        self.mana = mana
+import random
 
-        self.is_alive = True
+class Tamagotchi:
+
+    def __init__(self,name):
+        self.name = name
+        self.hunger = 50
+        self.energy = 50
+        self.happiness = 50
+        self.age = 0
+
+    def status(self):
+        print(f"1. {self.name}'s status")
+        print(f"2. Hunger: {self.hunger}/100")
+        print(f"3. Energy: {self.energy}/100")
+        print(f"4. Happiness: {self.happiness}/100")
+        print(f"5. Age: {self.age} days")
+
+    def feed(self):
+        print(f"\nYou Fed {self.name}.")
+        self.hunger -= 15
+        if self.hunger < 0 :
+            self.hunger == 0
+        if random.randint(-5, 10) >= 1:
+            self.happiness -= 15
+        else:
+            self.happiness += 5
+        if self.happiness < 0 :
+            self.happiness == 0
+            print(f"{self.name} is chronically depressesd and died of loneliness.")
+            return True
+    
+    def play(self):
+        print(f"\nYou played with {self.name}.")
+        self.happiness += random.randint(10,15)
+        self.hunger += random.randint(5,10)
+        self.energy -= random.randint(5,15)
+
+        if self.happiness > 100:
+            self.happiness == 100
+        if self.hunger > 100:
+            self.hunger == 100
+            print(f"{self.name} died of hunger.")
+            return True
+        if self.energy < 0:
+            self.energy == 0
+            print(f"{self.name} died of energy loss.")
+            return True
+
+    def sleep(self):
+        print(f"\nYou put {self.name} to sleep.")
+        self.energy += 25
+        self.hunger += random.randint(20,35)
+
+        if self.energy > 100:
+            self.energy == 100
+        if self.hunger > 100:
+            self.hunger == 100
+            print(f"{self.name} died of hunger.")
+            return True
+
+    def grow_older(self):
+        self.age += 1
+
+        if self.hunger > 100:
+            self.hunger == 100
+            print(f"{self.name} died of hunger.")
+            return True
+        if self.happiness < 0 :
+            self.happiness == 0
+            print(f"{self.name} is chronically depressesd and died of loneliness.")
+            return True
+
+        if self.age >= 15:
+            print(f"{self.name} grew up and left you. You won!")
+            return True
+
+    def abuse(self):
+        print(f"Choose a way to abuse {self.name}:")
+        print("\n1. Smack")
+        print("2. Shoot")
+        print("3. Throw")
+        print("4. Kill")
+        choice = input("Input your abuse method: ")
+
+    def smack(self):
+        print(f"You smacked {self.name}.")
+        self.happiness -= 30
+
+        if self.happiness < 0 :
+            self.happiness == 0
+
+    def shoot(self):
+        print("What weapons would you like to use?")
+        print(f"\n1. Bow")
+        print(f"2. Glock")
+        print(f"3. AK-47")
+        print(f"4. Darts")
+        choice = input("Pick your weapon: ")
+        print(f"\nYou almost killed {self.name} with a {choice}!")
+        self.happiness -= 70
+
+# Main code
+name = input("What is your tamagotchi's name? ")
+tamagotchi = Tamagotchi(name)
+while True:
+    
